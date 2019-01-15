3acfb5cc9345f4df22ce76a1fe385c545effca56
shadiao <1498800286@qq.com>
2019-01-15 21:39:48 +0800

    增加敏感字符

--- main.c
+++ main.c
@@ -15,7 +15,17 @@ int main()
         scanf("%d",&code);
 
         if(code == 1){
-            
+            printf("qingshuryaotianjiademinganzifu\n");
+            char m;
+            scanf("%c",&m);
+            scanf("%c",&m);
+            arr[index] = m;
+            index++;
+            printf("tianjiachenggong,dianjihuichejixu\n");
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
+
         }
         if(code == 2){
             
