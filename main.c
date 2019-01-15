95c89adde48a2022b6429d41cea2638fdb1b8802
shadiao <1498800286@qq.com>
2019-01-15 21:37:05 +0800

    基本框架

--- main.c
+++ main.c
@@ -1,8 +1,36 @@
-#include <stdio.h>
+int main()
+{
+    printf("minganzifuguanlixitong\n");
+    while(1)
+    {
+        printf("1---tianjiaminganzifu\n");
+        printf("2---shanchuminganzifu（zuihouyige）\n");
+        printf("3---chankansuoyouminganzifu\n");
+        printf("4---tihuanneirongzhongdeminganzifu\n");
+        printf("5---tuichu\n");
 
-int main(){
+        printf("qingxuanzexiangyinggongneng\n");
 
-    printf("hello world!");
+        int code;
+        scanf("%d",&code);
+
+        if(code == 1){
+            
+        }
+        if(code == 2){
+            
+        }
+        if(code == 3){
+           
+        }
+        if(code == 4){
+            
+        }
+        if(code == 5){
+           
+        }
+
+    }
 
     return 0;
 
